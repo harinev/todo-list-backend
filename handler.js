1b0fb9b941d0bb2be734a165ea07ad8b1c5860a8
harine <harinev@outlook.com>
2020-03-15 18:01:22 +0000

    backend hooked with db

--- handler.js
+++ handler.js
@@ -8,67 +8,28 @@ const app=express();
 app.use(cors());
 app.use(bodyParser.json())
 
-app.get("/tasks", function (request, response){
-	console.log(request);
+const mysql = require("mysql")
 
-	response.status(200).json({
-		Task: [
-			{
-			  id: 1,
-			  taskitem: "Pay Insurance",
-			  duedt: "2019/12/15",
-			  Completed: false
-			},
-	  
-			{
-			  id: 2,
-			  taskitem: "Pack the luggage",
-			  duedt: "2019/12/10",
-			  Completed: true
-			},
-			{
-			  id: 3,
-			  taskitem: "Do Shopping",
-			  duedt: "2019/12/14",
-			  Completed: false
-			},
-			{
-			  id: 4,
-			  taskitem: "Book Tickets for travel",
-			  duedt: "2019/08/15",
-			  Completed: true
-			},
-			{
-			  id: 5,
-			  taskitem: "New Year Party 2020",
-			  duedt: "2019/12/30",
-			  Completed: false
-			}
-		  ]
-	})
+const connection = mysql.createConnection({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: "tasks"
 })
-// const mysql = require("mysql")
-
-// const connection = mysql.createConnection({
-// 	host: process.env.DB_HOST,
-// 	user: process.env.DB_USER,
-// 	password: process.env.DB_PASSWORD,
-// 	database: "tasks"
-// })
-// app.get("/tasks", function(request, response){
-//     connection.query("SELECT * FROM Task", function (err, data){
-//         if(err){
-//             response.status(500).json({
-//                 error: err
+app.get("/tasks", function( response){
+    connection.query("SELECT * FROM Task", function (err, data){
+        if(err){
+            response.status(500).json({
+                error: err
                 
-//             })
-//         } else {
-//             response.status(200).json({
-//                 tasks: data
-//             });
-//         }
-//     })
-// });
+            })
+        } else {
+            response.status(200).json({
+                tasks: data
+            });
+        }
+    })
+});
 
 //put task
 
@@ -77,7 +38,7 @@ app.put("/tasks/:id", (request, response) => {
 	const updatedTask = request.body;
 	const id = request.params.id;
 
-	connection.query("UPDATE Task SET Completed = ? taskitem = ? duedt = ?  userid = ? WHERE id = ?", [updatedTask.Completed, updatedTask.duedt, updatedTask.taskitem, updatedTask.userid, id],
+	connection.query("UPDATE Task SET Completed = ? taskitem = ? duedt = ?  userid = ? WHERE id = ?", [updatedTask, id],
 		function (err) {
 			if (err) {
 				response.status(500).json({ error: err });
@@ -122,4 +83,4 @@ app.delete("/tasks/:id", (request, response) => {
 
 
 
-module.exports.tasks = serverlessHttp(app);
+module.exports.tasks = serverlessHttp(tasks);
