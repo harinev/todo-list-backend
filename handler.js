4a05e3489e941f34102ab8fbbb57f7843a07c39f
harine <harinev@outlook.com>
2020-03-15 13:33:36 +0000

    backend created

---
+++ handler.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const cors= require("cors");
+const serverlessHttp = require("serverless-http");
+
+const app=express();
+
+app.get("/todo", function(request, response){
+
+})
